64726da14d4148e2e9d6fe245acea6a03d835f27
Yra-A <1325083019@qq.com>
2023-10-25 23:21:42 +0800

    feat: contest and homeview 1. 修复调整了 NavBar 的一些样式 2. 增加了 404 页面 3. 实现了 contest 板块界面，并添加了相关请求，带有分页、筛选功能 TODO：实现 contest 板块界面的收藏功能

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,8 @@
-const server_url = 'http://127.0.0.1:4523/m1/3429271-0-default'
+const server_url = 'https://mock.apifox.cn/m1/3429271-0-default'
 
 const user_login_url = '/user/login'
 const user_info_url = '/user/info'
 
-export { server_url, user_info_url, user_login_url }
+const contest_list_url = '/contest/list'
+
+export { server_url, user_info_url, user_login_url, contest_list_url }
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,15 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       // component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/404/',
+      name: '404',
+      component: NotFoundView
+    },
+    {
+      path: '/:catchAll(.*)',
+      redirect: '/404/'
     }
   ]
 })
