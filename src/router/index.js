ce165fa7c7cc42d3a2685a97617bcc42d99562e8
gpqd123 <gpqd123@163.com>
2023-10-29 23:02:54 +0800

    feat:增加了用户档案和上传档案的界面样式 1.增加了用户档案界面 2.增加了上传档案界面

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import UserProfileView from '../views/UserProfileView.vue'
+import UserProfileUploadView from '../views/UserProfileUploadView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +30,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       // component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/user/profile',
+      name: 'userprofile',
+      component: UserProfileView
+    },
+    {
+      path: '/user/profile/upload',
+      name: 'userprofileupload',
+      component: UserProfileUploadView
     }
   ]
 })
