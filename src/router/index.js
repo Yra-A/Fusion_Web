cf3c275fe5e7915ddf970ed5bca6d94e7214024a
gpqd123 <gpqd123@163.com>
2023-10-30 11:41:34 +0800

    fix:解决router中的本地冲突 1.解决router中的本地冲突

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-<<<<<<< HEAD
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import UserProfileView from '../views/UserProfileView.vue'
-import UserProfileUploadView from '../views/UserProfileUploadView.vue'
-=======
+
 const HomeView = () => import('../views/HomeView.vue')
 const LoginView = () => import('../views/LoginView.vue')
 const RegisterView = () => import('../views/RegisterView.vue')
 const NotFoundView = () => import('../views/NotFoundView.vue')
 const ContestView = () => import('../views/ContestView.vue')
->>>>>>> d885ee46d4dc3e3587f876a7990febf40919e8be
+const UserProfileView = () => import('../views/UserProfileView.vue')
+const UserProfileUploadView = () => import('../views/UserProfileUploadView.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,15 +27,6 @@ const router = createRouter({
       component: RegisterView
     },
     {
-<<<<<<< HEAD
-      path: '/about',
-      name: 'about'
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/user/profile',
       name: 'userprofile',
       component: UserProfileView
@@ -49,7 +35,8 @@ const router = createRouter({
       path: '/user/profile/upload',
       name: 'userprofileupload',
       component: UserProfileUploadView
-=======
+    },
+    {
       path: '/contest/:contest_id/',
       name: 'contest',
       component: ContestView
@@ -58,7 +45,6 @@ const router = createRouter({
       path: '/404/',
       name: '404',
       component: NotFoundView
->>>>>>> d885ee46d4dc3e3587f876a7990febf40919e8be
     }
     // {
     //   path: '/:catchAll(.*)',
